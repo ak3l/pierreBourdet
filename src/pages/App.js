cd446bbe9ddd86a2874f99228d3b7b5ce1404d49
ak3l <pierre.bourdet1@gmail.com>
2019-12-08 18:28:41 +0100

    Add translations

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import Button from '@material-ui/core/Button'
+import translations from '../translations';
 import Header from '../components/Header';
 import './App.css';
 
-const App = () => (
-    <div className="App">
-        <Header />
-    </div>
-);
+const App = () => {
+    const [locale, changeLocale] = React.useState('fr');
+
+    return (
+        <IntlProvider locale={locale} messages={translations[locale]}>
+            <Button size="small" variant="contained" onClick={() => changeLocale('fr')}>FR</Button>
+            <Button size="small" variant="contained" onClick={() => changeLocale('en')}>EN</Button>
+            <FormattedMessage id="test.welcome" />
+            <div className="App">
+                <Header />
+            </div>
+        </IntlProvider>
+    );
+};
 
 export default App;
