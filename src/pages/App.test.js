ba4004ec396e9e5956e1714b8eef100046fc2e28
ak3l <pierre.bourdet1@gmail.com>
2019-12-08 18:01:38 +0100

    Add tests

--- src/pages/App.test.js
+++ src/pages/App.test.js
@@ -1,9 +1,8 @@
+import { shallow } from 'enzyme';
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+test('App', () => {
+    const component = shallow(<App/>);
+    expect(component).toMatchSnapshot();
 });
